4927947119efd3b41c16162ab155352dff42fe56
王鑫 <wangxin@souche.com>
2017-07-22 10:56:23 +0800

    change GestureDetector to OnClickListener

--- app/src/main/java/wang/relish/practice/mp3/MainActivity.java
+++ app/src/main/java/wang/relish/practice/mp3/MainActivity.java
@@ -1,12 +1,18 @@
 package wang.relish.practice.mp3;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,14 +51,6 @@ public class MainActivity extends AppCompatActivity {
         ImageAdapter adapter = new ImageAdapter(mMP3s);
         mViewPager.setAdapter(adapter);
 
-        final GestureDetector tapGestureDetector = new GestureDetector(this, new TapGestureListener());
-        mViewPager.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                tapGestureDetector.onTouchEvent(event);
-                return false;
-            }
-        });
-
         mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
@@ -76,43 +74,87 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private class TapGestureListener extends GestureDetector.SimpleOnGestureListener {
+    private class ImageAdapter extends PagerAdapter {
+
+        List<Integer> mData;
+
+        ImageAdapter(List<Integer> data) {
+            mData = data;
+        }
+
         @Override
-        public boolean onSingleTapConfirmed(MotionEvent e) {
-            int position = mViewPager.getCurrentItem();
-            if (mp != null) {
-                if (isPaused) {
-                    if (pausedPage == position) {
-                        mp.start();
-                        tv.append("page" + position + " CONTINUE\n");
+        public int getCount() {
+            return mData.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == ((ImageView) object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            Context context = container.getContext();// use this context is enough
+            ImageView imageView = new ImageView(context);
+            int padding = context.getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin);
+            imageView.setPadding(padding, padding, padding, padding);
+            imageView.setScaleType(ImageView.ScaleType.CENTER);
+            imageView.setImageBitmap(rawResIdToBitmap(context, mData.get(position)));//use the image album of the song
+            container.addView(imageView, 0);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = mViewPager.getCurrentItem();
+                    if (mp != null) {
+                        if (isPaused) {
+                            if (pausedPage == position) {
+                                mp.start();
+                                tv.append("page" + position + " CONTINUE\n");
+                            } else {
+                                mp = MediaPlayer.create(MainActivity.this, mMP3s.get(position));
+                                mp.start();
+                                tv.append("START page" + position + "\n");
+                            }
+                            isPaused = false;
+                        } else {
+                            mp.pause();
+                            isPaused = true;
+                            pausedPage = position;
+                            tv.append("PAUSE page" + position + "\n");
+                        }
                     } else {
                         mp = MediaPlayer.create(MainActivity.this, mMP3s.get(position));
                         mp.start();
                         tv.append("START page" + position + "\n");
+                        isPaused = false;
                     }
-                    isPaused = false;
-                } else {
-                    mp.pause();
-                    isPaused = true;
-                    pausedPage = position;
-                    tv.append("PAUSE page" + position + "\n");
-                }
-            } else {
-                mp = MediaPlayer.create(MainActivity.this, mMP3s.get(position));
-                mp.start();
-                tv.append("START page" + position + "\n");
-                isPaused = false;
-            }
-            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                public void onCompletion(MediaPlayer m) {
-                    Toast.makeText(MainActivity.this,
-                            "COMPLETED", Toast.LENGTH_LONG).show();
-                    // Set the MainActivity member to null
-                    MainActivity.this.mp = null;
+                    mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer m) {
+                            Toast.makeText(MainActivity.this,
+                                    "COMPLETED", Toast.LENGTH_LONG).show();
+                            // Set the MainActivity member to null
+                            MainActivity.this.mp = null;
+                        }
+                    });
                 }
             });
-            return super.onSingleTapConfirmed(e);
+            return imageView;
         }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((ImageView) object);
+        }
+    }
+
+    /**
+     * fetch the bitmap(the image album of the song) from the raw resource id of the song
+     */
+    private static Bitmap rawResIdToBitmap(Context context, int rawResId) {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(context, Uri.parse("android.resource://" + context.getPackageName() + "/" + rawResId));
+        byte[] buffer = retriever.getEmbeddedPicture();
+        return BitmapFactory.decodeByteArray(buffer, 0, buffer.length);
     }
 }
 
